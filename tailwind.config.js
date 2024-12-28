40acc571ae1e0b7a277ed898a6a1220f2af267e7
Yalina Gómez <yalinita1985@gmail.com>
2024-12-28 02:46:08 +0100

    responsive with profile photo

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
       },
       screens: {
         'xs': '320px',
-        'custom-md': { min: '920px', max: '1023px' },
+        'custom-md': { min: '704px', max: '1023px' },
       },
     },
   },
